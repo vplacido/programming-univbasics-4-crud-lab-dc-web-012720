0c229af48d87c5076bcdf7469613a05cc86981de
Valentin Placido <valentinplacido25@gmail.com>
2020-01-15 20:59:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr = [1]
+  arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -29,17 +29,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[array.size]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
