31507eebcc3d944fedafe8fcbdd882682615f82b
Valentin Placido <valentinplacido25@gmail.com>
2020-01-15 20:54:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  if (array.size != 0) {
+    array.shift()
+  }
 end
 
 def retrieve_element_from_index(array, index_number)
